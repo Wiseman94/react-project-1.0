ba471681e6e902a5c3a30efde9b7e5e4ce7052b7
Konstantin Mudriy <konstantin.mudriy@gmail.com>
2016-08-30 11:31:53 +0300

    add gitignore

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
-/**
- * Created by Kostyantin on 8/30/2016.
- */
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 3000));
+
+app.use('/', express.static(__dirname));
+
+app.listen(app.get('port'), function () {
+    console.log('Server	started:	http://localhost:' + app.get('port') + '/');
+});
